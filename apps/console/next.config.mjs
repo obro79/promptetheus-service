9df1c76fcc9f08c753a887ac4c10217f3b13b103
Saksham Adhikari <125867301+Tar-ive@users.noreply.github.com>
2026-06-20 21:32:06 -0700

    ci: revert hoisted, set outputFileTracingRoot to repo root for pnpm trace (#16)
    Co-authored-by: Saksham Adhikari <sadhikari@askslm.com>

--- apps/console/next.config.mjs
+++ apps/console/next.config.mjs
@@ -1,8 +1,13 @@
+import path from "node:path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
   distDir: process.env.NEXT_DIST_DIR ?? ".next",
+  experimental: {
+    outputFileTracingRoot: path.join(process.cwd(), "../../"),
+  },
 };
 
 export default nextConfig;
